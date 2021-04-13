c96b79f6e183d4bdb3c48dcaabbfeba2c6c5866f
Gordon Shieh <gordon.shieh94@gmail.com>
2021-04-12 22:53:40 -0700

    Named layers

--- keyboards/whitefox/keymaps/hhkb/keymap.c
+++ keyboards/whitefox/keymaps/hhkb/keymap.c
@@ -16,7 +16,16 @@ along with this program.`` If not, see <http://www.gnu.org/licenses/>.
 #include QMK_KEYBOARD_H
 
 enum {
-    TD_SPC_L4
+    _BASE = 0,
+    _MODIFIER,
+    _VOLUME,
+    _SPACE_HOLD
+};
+
+enum {
+    TD_SPC,
+    TD_LCTL,
+    TD_RCTL
 };
 
 typedef enum {
@@ -34,6 +43,9 @@ td_state_t cur_dance(qk_tap_dance_state_t *state);
 void spc_finished(qk_tap_dance_state_t *state, void *user_data);
 void spc_reset(qk_tap_dance_state_t *state, void *user_data);
 
+void lctrl_finished(qk_tap_dance_state_t *state, void *user_data);
+void lctrl_reset(qk_tap_dance_state_t *state, void *user_data);
+
 static td_tap_t spc_tap_state = {
     .is_press_action = true,
     .state = TD_NONE
@@ -42,7 +54,8 @@ static td_tap_t spc_tap_state = {
 // Tap Dance definitions
 qk_tap_dance_action_t tap_dance_actions[] = {
     // Tap once for Escape, twice for Caps Lock
-    [TD_SPC_L4] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, spc_finished, spc_reset),
+    [TD_SPC] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, spc_finished, spc_reset),
+    [TD_LCTL] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, lctrl_finished, lctrl_reset),
 };
 
 const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -59,20 +72,20 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |Ctrl|Gui |Alt |         Space    |Fn0 |Alt |Gui |  |Lef|Dow|Rig|
      * `---------------------------------------------------------------'
      */
-    [0] = LAYOUT_truefox( \
+    [_BASE] = LAYOUT_truefox( \
         KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV, MO(2), \
         KC_TAB,  KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P,    KC_LBRC, KC_RBRC, KC_BSPC, KC_DEL, \
         KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,  KC_PGUP, \
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_PGDN, \
-        TT(1), KC_LGUI, KC_LALT, TD(TD_SPC_L4), KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT \
+        TT(_MODIFIER), KC_LGUI, KC_LALT, TD(TD_SPC), KC_RALT, MO(_VOLUME), KC_LEFT, KC_DOWN, KC_RGHT \
     ),
     // Modifier keys
-    [1] = LAYOUT_truefox( \
-        TO(0), KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, KC_TRNS, KC_TRNS, \
+    [_MODIFIER] = LAYOUT_truefox( \
+        TO(_BASE), KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, KC_TRNS, KC_TRNS, \
         KC_TRNS, KC_TRNS, KC_UP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL, KC_INS, \
-        KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, \
+        TD(TD_LCTL), KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, \
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_END, \
-        TO(0), MO(3), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS \
+        TO(_BASE), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS \
     ),
     // Non-existent brightness controls
     [2] = LAYOUT_truefox( \
@@ -83,15 +96,15 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_TRNS, KC_TRNS, BL_TOGG, BL_STEP, BL_BRTG, KC_TRNS, BL_DEC, KC_TRNS \
     ),
     // Audio controls
-    [3] = LAYOUT_truefox( \
+    [_VOLUME] = LAYOUT_truefox( \
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPLY, KC_TRNS, \
+        KC_TRNS, KC_TRNS, KC_VOLU, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+        KC_TRNS, KC_MPRV, KC_VOLD, KC_MNXT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPLY, KC_TRNS, \
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLU, KC_TRNS, \
         KC_TRNS, KC_TRNS, KC_TRNS, KC_MUTE, KC_TRNS, KC_TRNS, KC_MPRV, KC_VOLD, KC_MNXT \
     ),
     // Space hold
-    [4] = LAYOUT_truefox( \
+    [_SPACE_HOLD] = LAYOUT_truefox( \
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL, KC_TRNS, \
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
@@ -115,7 +128,7 @@ void spc_finished(qk_tap_dance_state_t *state, void *user_data) {
             tap_code(KC_SPC);
             break;
         case TD_SINGLE_HOLD:
-            layer_on(4);
+            layer_on(_SPACE_HOLD);
             break;
         default: break;
     }
@@ -123,7 +136,27 @@ void spc_finished(qk_tap_dance_state_t *state, void *user_data) {
 
 void spc_reset(qk_tap_dance_state_t *state, void *user_data) {
     if (spc_tap_state.state == TD_SINGLE_HOLD) {
-        layer_off(4);
+        layer_off(_SPACE_HOLD);
+    }
+    spc_tap_state.state = TD_NONE;
+}
+
+void lctrl_finished(qk_tap_dance_state_t *state, void *user_data) {
+    spc_tap_state.state = cur_dance(state);
+    switch (spc_tap_state.state) {
+        case TD_SINGLE_TAP:
+            tap_code(KC_CAPS);
+            break;
+        case TD_SINGLE_HOLD:
+            register_code(KC_LCTL);
+            break;
+        default: break;
+    }
+}
+
+void lctrl_reset(qk_tap_dance_state_t *state, void *user_data) {
+    if (spc_tap_state.state == TD_SINGLE_HOLD) {
+        unregister_code(KC_LCTL);
     }
     spc_tap_state.state = TD_NONE;
 }
